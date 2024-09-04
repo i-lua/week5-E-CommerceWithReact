135dddc5fdfd8c5959aa94587aa90a0bd4dd8d76
Issac <issaclua@gmail.com>
2024-09-03 20:45:36 -0700

    started to implement books

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import Landing from './components/Landing';
 import Nav from './components/Nav'
+import Highlights from './components/Highlights'
+import Featured from './components/Featured';
 
 function App() {
   return (
     <div className="App">
       <Nav />
       <Landing />
+      <Highlights />
+      <Featured />
     </div>
   );
 }
